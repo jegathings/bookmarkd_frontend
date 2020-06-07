656f589b3cfcd30708370198129d8d910cc5ebc7
James Gathings <jegathings@gmail.com>
2020-06-07 13:17:01 -0400

    Updated the routes to use the modified bookmarkd api.

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ const App = (props) => {
     
 
     const getInfo = async() =>{
-        const response = await fetch(`http://localhost:3000/bookmarks`);
+        const response = await fetch(`http://localhost:3000/bookmarks/index`);
         const result = await response.json();
         setBookmarks(result);
     }
@@ -33,7 +33,7 @@ const App = (props) => {
     },[]);
 
     const handleCreate = async (data) => {
-        const response = await fetch('http://localhost:3000/bookmarks', {
+        const response = await fetch('http://localhost:3000/bookmarks/create', {
             method: 'POST',
             headers: {
                 'Content-type': 'application/json',
@@ -51,7 +51,7 @@ const App = (props) => {
     const handleEdit = async (data) => {
         //updates the selected holiday
         const response = await fetch(
-            `http://localhost:3000/bookmarks/${data._id}`,
+            `http://localhost:3000/bookmarks/update/${data._id}`,
             {
                 method: 'PUT',
                 headers: {
@@ -67,7 +67,7 @@ const App = (props) => {
 
     const handleDelete = async (data) =>{
         const respone = await fetch(
-            `http://localhost:3000/bookmarks/${data._id}`,
+            `http://localhost:3000/bookmarks/delete/${data._id}`,
             {
                 method: 'DELETE',
                 headers: {'Content-Type': 'application/json',
