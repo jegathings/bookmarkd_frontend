84fa9e95ae04015f778ec52f868930aac45396de
PhilCodesNow <philipmayojr@outlook.com>
2020-06-07 11:51:02 -0400

    copied files from started project folder

--- src/app.js
+++ src/app.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './css/style.css';
+import './css/style.scss';
+import Form from './components/NewForm'
+
+
 
 const App = (props) => {
-    const blank = {title:'', url:''};
+    // const addBookmarkPlaceholder = 'Add Bookmark';
+    // const EditBookmarkPlaceholder = 'Edit';
     const [bookmarks, setBookmarks] = React.useState(null);
-    const baseURL = 'https://assembled-bookmarks.herokuapp.com';
+    /////// sets state for editing
+    const [editBookmark, setEditBookmark] = React.useState({
+        title: '',
+        url: '',
+    });
+    const [blankForm, setBlankForm] = React.useState({
+        title: '',
+        url: '',
+    })
+    
+    const blank = {title:'', url:''};
+    
 
     const getInfo = async() =>{
-        const response = await fetch(`${baseURL}/bookmarks/index`);
+        const response = await fetch(`http://localhost:3000/bookmarks`);
         const result = await response.json();
         setBookmarks(result);
     }
@@ -17,16 +32,87 @@ const App = (props) => {
         getInfo()
     },[]);
 
+    const handleCreate = async (data) => {
+        const response = await fetch('http://localhost:3000/bookmarks', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+        getInfo();
+        setBlankForm;
+    }
+
+    const handleSelect = async (bookmark) =>{
+        setEditBookmark(bookmark);
+    };
+
+    const handleEdit = async (data) => {
+        //updates the selected holiday
+        const response = await fetch(
+            `http://localhost:3000/bookmarks/${data._id}`,
+            {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            }
+        );
+        //grab the updated list of holidays
+        getInfo();
+
+    };
+
+    const handleDelete = async (data) =>{
+        const respone = await fetch(
+            `http://localhost:3000/bookmarks/${data._id}`,
+            {
+                method: 'DELETE',
+                headers: {'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+            }
+        )
+        getInfo();
+    }
+
     return (
         <>
+        <div className="main">
             <h1>Bookmarkd</h1>
+            <div>
+                <h3>Add A Bookmark</h3>
+                <Form initial={blank} handleSubmit = {handleCreate}/>
+            </div>
+            <div>
+            <h3>Edit A Bookmark</h3>
+            <Form initial={editBookmark} handleSubmit={handleEdit} resetForm={blank}/>
+            </div>
             <ul>
                 {
                     bookmarks ? 
                     bookmarks.map((bookmark, index) => {
                         return(
-                            <li>
+                            <li key={bookmark._id} className="main-list-item">
+                                <div className="main-list-item-div main-list-item-div-one">
                                 <h1><a href={bookmark.url} target="_blank">{bookmark.title}</a></h1>
+                                </div>
+                                <div className="main-list-item-div main-list-item-div-two">
+                                <button
+                                    className="main-list-btn"
+                                    onClick={() =>{
+                                        handleSelect(bookmark);
+                                    }}
+                                >&#9998;</button>
+                                <button
+                                    className="main-list-btn"
+                                    onClick={() =>{
+                                        handleDelete(bookmark);
+                                    }}
+                                >&#10007;</button>
+                                </div>
                             </li>
                         )
                     })
@@ -34,6 +120,7 @@ const App = (props) => {
                     "...Loading"
                 }
             </ul>
+            </div>
         </>
     );
 };
